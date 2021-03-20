552cef37006047ba68aba806d5090e39e20ae8f2
krishpranav <krisna.pranav@gmail.com>
2021-03-20 10:36:24 +0530

    done changes

--- include-services.py
+++ include-services.py
@@ -39,3 +39,44 @@ def parse_arguments():
     
     return parser.parse_args()
 
+def read_services(nmap_services_file):
+    print '[+] Reading services from \'%s\'' % nmap_services_file
+    sfile = csv.reader(open(nmap_services_file, 'r'), dialect='excel-tab')
+    services = []
+    for s in sfile:
+        if not str(s[0]).startswith('#'):
+            services.append((s[1], s[0], s[2]))
+    
+    return sorted(services, key=lambda s: s[2], reverse=True)
+
+def update_py(services, nmappy_file, number):
+    # Generate lists of top X TCP/UDP ports
+    if number != 0:
+        print '[+] Generating list of %s TCP/UDP ports' % ('top %d' % number if number < sys.maxint else 'all')
+    else:
+        print '[+] Removing list of TCP/UDP ports'
+    top_tcp = filter(lambda s: s[0].endswith('tcp'), services)[:number]
+    top_udp = filter(lambda s: s[0].endswith('udp'), services)[:number]
+    combined = sorted(top_tcp + top_udp, key=lambda s: s[2], reverse=True)
+    new_services = 'services = [%s%s\n]' % ('\n    ' if len(combined) > 0 else '', ',\n    '.join([str(s) for s in combined]))
+
+    # Replace list of services using regex
+    print '[+] Finding and replacing existing list of services'
+    with open(nmappy_file, 'r') as f:
+        script = f.read()
+    pattern = '^services = \[\n(    \(\'[0-9]{1,5}/[udtcp]{3}\', \'[ -~]+\', \'[0-9]\.[0-9]+\'\),?\n)*\]'
+    new_script = re.sub(pattern, new_services, script, flags=re.MULTILINE)
+
+    # Update the ALL_SERVICES_INCLUDED variable
+    tag = 'ALL_SERVICES_INCLUDED = %s'
+    if number == sys.maxint:
+        (b_old, b_new) = ('False', 'True')
+    else:
+        (b_old, b_new) = ('True', 'False')
+    new_script = new_script.replace(tag % b_old, tag % b_new)
+
+    # Update nmappy Python script
+    print '[+] Saving updated script to \'%s\'' % nmappy_file
+    with open(nmappy_file, 'w') as f:
+        f.write(new_script)
+    
