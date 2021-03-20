44d7fa34d593f154e6f4e3b1d6f7285373eb0886
krishpranav <krisna.pranav@gmail.com>
2021-03-20 10:43:36 +0530

    done changes

--- topports.py
+++ topports.py
@@ -10,3 +10,24 @@ def read_services(namp_services_file):
             services.append((s[1], s[0], s[2]))
     
     return sorted(services, key=lambda s: s[2], reverse=True)
+
+def main():
+    if len(sys.argv) == 1:
+        print 'usage: topports.py [top # of ports] [optional: tcp|udp]'
+        print 'example: topports.py 10 tcp\n'
+        print 'TopPorts v1.0 ( https://github.com/bitsadmin/nmappy/ )'
+        return -1
+    
+    max = 10
+    type = 'tcp'
+    if len(sys.argv) > 1:
+        max = int(sys.argv[1])
+    if len(sys.argv) > 2:
+        type = sys.argv[2]
+    
+    services = read_services('nmap-services')
+    services = filter(lambda s: s[0].endswith(type), services)
+
+    print ','.join([s[0].split('/')[0] for s in services[0:max]])
+
+if __name__ = '__main__': main()
