d87a9f70e72658bec5aa0b18409224b6c8e12822
krishpranav <krisna.pranav@gmail.com>
2021-03-20 10:27:53 +0530

    done chagnes

--- include-services.py
+++ include-services.py
@@ -26,3 +26,16 @@ def validate_number(value):
         i = sys.maxint
     
     return i
+
+def parse_arguments():
+    parser = argparse.ArgumentParser(description='NmapPy services includer %.2f ( %s )' % (VERSION, WEB_URL))
+    parser.add_argument('nmappy_file', action='store', type=validate_file, default='nmappy.py', nargs='?', help='File to patch')
+    parser.add_argument('nmap_services_file', action='store', type=validate_file, default='nmap-services', nargs='?', help='nmap-services source file')
+    parser.add_argument('-i', '--include', dest='number', action='store', type=validate_number, default=50, help='Number of TCP and UDP services to include (default: 50). Use -1 for all.')
+
+    if len(sys.argv) == 1:
+        parser.print_help()
+        sys.exit(1)
+    
+    return parser.parse_args()
+
